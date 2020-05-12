502364d6a4b8da77ef2fe1cce86d70800e7285be
Nick <legokol3@gmail.com>
2020-05-12 19:22:54 +0300

    Reworked collision.

--- DrawableParticle.cpp
+++ DrawableParticle.cpp
@@ -8,6 +8,10 @@ void DrawableParticle::update(double dt, sf::RenderWindow &w) {
     move(dt);
     setLoc(loc);
     display(w);
+    if (loc.getX() < r || loc.getX() > (double)w.getSize().x - r)
+        v.setX(-v.getX());
+    if (loc.getY() < r || loc.getY() > (double)w.getSize().y - r)
+        v.setY(-v.getY());
 }
 
 void DrawableParticle::setLoc(const Vector2D &_loc) {
